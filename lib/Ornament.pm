22d2c4ec7433961e5476351200d892e24be183ce
shelling <navyblueshellingford@gmail.com>
2013-06-09 14:59:25 +0800

    add basic project information

--- lib/Ornament.pm
+++ lib/Ornament.pm
@@ -26,7 +26,6 @@ shelling E<lt>navyblueshellingford@gmail.comE<gt>
 
 =head1 LICENSE
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+MIT (X11) License
 
 =cut
